e5da5579393202264b8b8cb1820b6b375990de37
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-04-17 11:20:18 +0530

    Add 'App' component

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 
+import './app.css';
+
 class App extends Component {
   render() {
     return (
-        <div className="app">Welcome to React</div>
+      <React.Fragment>
+        <div className="app">
+          <h1>Welcome to the React Application</h1>
+        </div>
+      </React.Fragment>
     );
   }
 }
